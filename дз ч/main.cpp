0a66a3e9cce39bfb627fabd73504238ea5986a10
Vadik228725 <81514134+Vadik228725@users.noreply.github.com>
2021-03-28 17:30:08 +0500

    Оформатировал код

--- дз ч/main.cpp
+++ дз ч/main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
- bool palindrom(char s[100]);
+bool palindrom(char s[100]);
 int main()
 {
     char s[100];
     cin.getline(s, 100);
- if (palindrom(s))
-    cout << "palendrom " << endl;
-else
-cout<<"ne palendrom "<<endl;
+    if (palindrom(s))
+        cout << "palendrom " << endl;
+    else
+        cout<<"ne palendrom "<<endl;
     return 0;
 }
- bool palindrom(char s[]){
-      int n = strlen(s);
- for (int i = 0;  i < n / 2; i++ )
-if(s[i]!=s[n-1-i])
-return false;
-return true;
+
+bool palindrom(char s[])
+{
+    int n = strlen(s);
+    for (int i = 0;  i < n / 2; i++ )
+        if(s[i]!=s[n-1-i])
+            return false;
+    return true;
 }
